5705286dd5fdc1050b700affa1d4e3ee806c3c22
Ikram Maulana <ikram075@ummi.ac.id>
2024-07-14 06:58:37 +0000

    chore(component): update navbar component
    - add active condition

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,28 @@
+"use client";
+
 import { nav } from "@/config";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { type FC } from "react";
 
 export const Navbar: FC = () => {
+  const pathname = usePathname();
+
   return (
     <header className="flex gap-2 p-2">
-      {nav.map((item) => (
-        <Link key={`Nav-${item.title}`} href={item.href}>
+      <Link
+        href={nav[0]?.href ?? ""}
+        className={pathname === "/" ? "font-bold" : ""}
+      >
+        {nav[0]?.title}
+      </Link>
+
+      {nav.slice(1).map((item) => (
+        <Link
+          key={`Nav-${item.title}`}
+          href={item.href}
+          className={pathname === item.href ? "font-bold" : ""}
+        >
           {item.title}
         </Link>
       ))}
