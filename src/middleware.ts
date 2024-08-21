bb4b37f887418cb08224cb410601ac1e4479f69d
Ikram-Maulana <ikram075@ummi.ac.id>
2024-08-21 17:36:04 +0700

    chore(middleware): change from nexturl to url

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,17 +16,17 @@ export const middleware = async (req: NextRequest) => {
   // Custom logic for public, auth, and api routes
   if (isAPIRoute(req)) return NextResponse.next();
 
-  const { nextUrl } = req;
+  const { url } = req;
   const session = await auth();
   const isAuth = isAuthRoute(req);
   const isPublic = isPublicRoute(req);
 
   if (isAuth && session?.user)
-    return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, url));
   // The code below manipulates routes that are not public path and not contain the "default login redirect" path to be nexted and will be directed to not found page by Next.js
   if (!req.url.includes(DEFAULT_LOGIN_REDIRECT)) return NextResponse.next();
   if (!session?.user && !isPublic)
-    return NextResponse.redirect(new URL("/auth/login", nextUrl));
+    return NextResponse.redirect(new URL("/auth/login", url));
 
   return NextResponse.next();
 };
